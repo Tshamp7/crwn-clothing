6359b66e34836e6b3e123b92b97eb119a64c8417
Tom Shamp <T.shamp7@gmail.com>
2020-10-01 15:17:51 -0700

    CustomButton done

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-
+import FormInput from "../form-input/form-input.component";
 import "./sign-in.styles.scss";
 import SignInAndSignUpPage from "../../pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CustomButton from "../../components/custom-button/custom-button.component";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -30,23 +31,24 @@ class SignIn extends React.Component {
         <h2>I already Have an Account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name="email"
             type="email"
             value={this.state.email}
-            onChange={this.handleChange}
+            label="email"
+            handleChange={this.handleChange}
             required
           />
-          <label>Email</label>
-          <input
+          <FormInput
             name="password"
             type="password"
-            value={this.state.email}
-            onChange={this.handleChange}
+            value={this.state.password}
+            label="password"
+            handleChange={this.handleChange}
             required
           />
-          <label>Password</label>
-          <input type="submit" value="Submit Form" />
+
+          <CustomButton type="submit">Sign In</CustomButton>
         </form>
       </div>
     );

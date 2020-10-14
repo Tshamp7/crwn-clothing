a17b34f4e442f3a270471a216df06793fdcdd42a
Tom Shamp <T.shamp7@gmail.com>
2020-10-14 16:00:14 -0700

    Add increase and decrease quantity to cart

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { clearItemFromCart } from "../../redux/cart/cart.actions";
+import { clearItemFromCart, addItem, removeItem } from "../../redux/cart/cart.actions";
 import "./checkout-item.styles.scss"
 
 const mapDispatchToProps = dispatch => ({
-    clearItem: item => dispatch(clearItemFromCart(item))
+    clearItemFromCart: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
 })
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItemFromCart, addItem, removeItem }) => {
 
     const { name, imageUrl, price, quantity } = cartItem;
 
@@ -17,9 +19,13 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
                 <img src={imageUrl} alt='item' />
             </div>
             <span className="name">{name}</span>
-            <span className="quantity">{quantity}</span>
+            <span className="quantity">
+                <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
+                <span className="value"> {quantity} </span>
+                <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
+            </span>
             <span className="price">{price}</span>
-            <div className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</div>
+            <div className="remove-button" onClick={() => clearItemFromCart(cartItem)}>&#10005;</div>
         </div>
     )
 };
